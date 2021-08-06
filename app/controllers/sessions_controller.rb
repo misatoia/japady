8de082a4e9ebcee066053f4d2aec87ce25b4a442
sumret <sumret@gmail.com>
2021-08-06 04:12:18 +0000

    retry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -84,16 +84,16 @@ class SessionsController < ApplicationController
       uri = URI(api_base_url + '?' + params1.map{|k,v| "#{k}=#{v}"}.join('&'))
       response_json = Net::HTTP.get(uri)
       @response_data = JSON.parse(response_json)
-      user_token = @response_data['access_token']
+      @user_token = @response_data['access_token']
 
       # アクセストークンの検査 -> アクセストークン情報を取得
-#      debug_token_url='graph.facebook.com/oauth/debug_token'
-#      params2 = {
-#        'input_token' => user_token,
-#        'access_token' => "#{facebook_client_id}|#{ENV['FACEBOOK_API_SECRET']}"
-#      }
-#      uri2 = URI(debug_token_url + '?' + params2.map{|k,v| "#{k}=#{v}"}.join('&'))
-#      response_json2 = Net::HTTP.get(uri2)
+      debug_token_url='graph.facebook.com/oauth/debug_token'
+      params2 = {
+        'input_token' => user_token,
+        'access_token' => "#{facebook_client_id}|#{ENV['FACEBOOK_API_SECRET']}"
+      }
+      uri2 = URI(debug_token_url + '?' + params2.map{|k,v| "#{k}=#{v}"}.join('&'))
+      response_json2 = Net::HTTP.get(uri2)
 #      @response_data2 = JSON.parse(response_json2)
 #      user_id = response_data2['data']['user_id']
       @response_data2 = []
