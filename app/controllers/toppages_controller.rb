84fd459abb3de91711ff30845db77ac43d50b2a9
sumret <sumret@gmail.com>
2021-08-25 12:55:21 +0000

    change buttons for notes and lessons

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -11,6 +11,7 @@ class ToppagesController < ApplicationController
     @days_of_recent = 30
     num_of_announcement = 3
     num_of_next_lesson = 1
+    @num_of_top_likes = 5
 
     @title = 'ダッシュボード'
 
@@ -28,6 +29,7 @@ class ToppagesController < ApplicationController
       @favorite_notes = current_user.favorite_notes.order(updated_at: :desc)
       @topliked_notes = Note.select('notes.*', 'count(likes.id) AS num_like').joins(:likes).group('notes.id')\
                             .order('num_like desc').where('notes.created_at > ?', Time.zone.now.ago(@days_of_recent.days))
+                            .limit(@num_of_top_likes)
       @following_notes = current_user.followings.map(&:latest_note).compact
     end
 
