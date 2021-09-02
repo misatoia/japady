5a148f354e30c4a91132cdfb27f2b1ee4c8e5801
sumret <sumret@gmail.com>
2021-09-02 13:40:49 +0000

    submit 20210902, correct grouping methods in dashboard for PostgreSQL

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -28,9 +28,16 @@ class ToppagesController < ApplicationController
                 
       @announcement = notes_with_users.where(announce: true).order(updated_at: :desc).first(num_of_announcement)
       @favorite_notes = current_user.favorite_notes.order(updated_at: :desc)
-      @topliked_notes = notes_with_users.select('notes.*', 'count(likes.id) AS num_like').joins(:likes).group('notes.id')\
-                            .order('num_like desc').where('notes.created_at > ?', Time.zone.now.ago(@days_of_recent.days))\
-                            .limit(@num_of_top_likes)
+      @topliked_notes = notes_with_users\
+                          .from(Note\
+                            .joins(:likes)\
+                            .group('notes.id')\
+                            .select('notes.*', 'count(likes.id) AS num_like')\
+                            , :notes)\
+                          .order('num_like desc')\
+                          .where('notes.created_at > ?', Time.zone.now.ago(@days_of_recent.days))\
+                          .limit(@num_of_top_likes)
+
       @following_notes = current_user.followings.map(&:latest_note).compact
     end
 
